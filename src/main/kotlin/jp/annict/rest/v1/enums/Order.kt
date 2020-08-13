83342abd212a5bab2c5eba8b5ebb934df213ef64
notsmatch <smatchjp@gmail.com>
2020-08-13 22:57:44 +0900

    fix: 小文字命名へ

---
+++ src/main/kotlin/jp/annict/rest/v1/enums/Order.kt
@@ -0,0 +1,5 @@
+package jp.annict.rest.v1.enums
+
+enum class Order {
+    asc, desc
+}
--- src/main/kotlin/jp/annict/rest/v1/type/Order.kt
+++
@@ -1,5 +0,0 @@
-package jp.annict.rest.v1.type
-
-enum class Order(val value: String) {
-    ASC("asc"), DESC("desc")
-}
