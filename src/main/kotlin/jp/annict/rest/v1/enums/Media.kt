848a3742973c0708200c5689e7982fb24b41dc6f
notsmatch <smatchjp@gmail.com>
2020-08-13 22:57:30 +0900

    feat: enums

---
+++ src/main/kotlin/jp/annict/rest/v1/enums/Media.kt
@@ -0,0 +1,10 @@
+package jp.annict.rest.v1.enums
+
+enum class Media {
+
+    tv,
+    ova,
+    movie,
+    web,
+    other
+}
---
+++ src/main/kotlin/jp/annict/rest/v1/enums/RatingState.kt
@@ -0,0 +1,9 @@
+package jp.annict.rest.v1.enums
+
+enum class RatingState {
+
+    bad,
+    average,
+    good,
+    great
+}
