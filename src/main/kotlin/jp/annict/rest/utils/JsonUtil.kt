6e5e1509c6b506057d2693ebade074aa3760ba8b
notsmatch <smatchjp@gmail.com>
2020-08-13 18:10:17 +0900

    feat

--- src/main/kotlin/jp/annict/rest/utils/JsonUtil.kt
+++ src/main/kotlin/jp/annict/rest/utils/JsonUtil.kt
@@ -3,12 +3,15 @@ package jp.annict.rest.utils
 import com.google.gson.FieldNamingPolicy
 import com.google.gson.GsonBuilder
 import com.google.gson.JsonParser
+import java.lang.reflect.Type
+import java.util.*
 
 
 object JsonUtil {
 
     val GSON = GsonBuilder()
         .setFieldNamingPolicy(FieldNamingPolicy.LOWER_CASE_WITH_UNDERSCORES)
+        .registerTypeAdapter(Date::class.java, DateDeserializer())
         .create()
 
     val JSON_PARSER = JsonParser()
