b6654c6da50165baa186d9a71cb9848a1286c143
riptakagi <smatchjp@gmail.com>
2020-08-14 18:07:25 +0900

    feat: RequestData: 扱いやすいように抽象化

---
+++ src/main/kotlin/jp/annict/rest/bases/RequestData.kt
@@ -0,0 +1,20 @@
+package jp.annict.rest.bases
+
+import jp.annict.rest.interfaces.AnnictClient
+import okhttp3.HttpUrl
+import okhttp3.Request
+import okhttp3.RequestBody
+
+/**
+ * GET以外のリクエストに利用できる
+ */
+abstract class RequestData(val method: String) {
+
+    abstract fun toRequestBody() : RequestBody
+
+    abstract fun url(client: AnnictClient): HttpUrl
+
+    fun toRequestBuilder(client: AnnictClient) : Request.Builder {
+        return Request.Builder().url(url(client)).method(method, toRequestBody())
+    }
+}
