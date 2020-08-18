4cf3ed1ab569f3d23b7004b72141a4a69937a274
riptakagi <smatchjp@gmail.com>
2020-08-18 11:09:22 +0900

    fix: 複数バージョンへの対応

--- src/main/kotlin/jp/annict/lib/AnnictLib.kt
+++ src/main/kotlin/jp/annict/lib/AnnictLib.kt
@@ -1,11 +1,16 @@
 package jp.annict.lib
 
+import jp.annict.lib.enums.AnnictVersion
 import jp.annict.lib.interfaces.AnnictClient
 import okhttp3.OkHttpClient
 
 object AnnictLib {
 
-    fun newClient(token: String, httpClient: OkHttpClient) : AnnictClient {
-        return jp.annict.lib.v1.client.AnnictClientImpl(token, httpClient)
+    fun newClient(token: String, httpClient: OkHttpClient, version: AnnictVersion) : AnnictClient {
+        when(version) {
+            AnnictVersion.V_1 -> {
+                return jp.annict.lib.v1.client.AnnictClientImpl(token, httpClient)
+            }
+        }
     }
 }
