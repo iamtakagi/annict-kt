c339542dd16ed0979afb5f7db2dcfb28d8d20bf8
riptakagi <smatchjp@gmail.com>
2020-08-18 13:09:01 +0900

    cleanup

--- src/main/kotlin/jp/annict/lib/bases/BaseRequestData.kt
+++ src/main/kotlin/jp/annict/lib/bases/BaseRequestData.kt
@@ -1,5 +1,6 @@
 package jp.annict.lib.bases
 
+import com.google.gson.JsonObject
 import jp.annict.lib.interfaces.AnnictClient
 import jp.annict.lib.utils.JsonUtil
 import okhttp3.HttpUrl
@@ -11,14 +12,16 @@ import okhttp3.RequestBody
 /**
  * GET以外のリクエストに利用できる
  */
-abstract class BaseRequestData(val method: String, val mediaType: MediaType, val path: String) {
+abstract class BaseRequestData(val method: String, val mediaType: String, val path: String) {
+
+    abstract fun toJsonObject() : JsonObject
 
     fun url(client: AnnictClient): HttpUrl {
        return  client.getUrlBuilder().addPathSegments(path).build()
     }
 
     fun toRequestBody(): RequestBody {
-        return RequestBody.create(mediaType, JsonUtil.GSON.toJson(this))
+        return RequestBody.create(mediaType.toMediaType(), toJsonObject().toString())
     }
 
     fun toRequestBuilder(client: AnnictClient) : Request.Builder {
