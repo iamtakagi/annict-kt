10ea47c1496cd465254453324f609fc708903745
riptakagi <smatchjp@gmail.com>
2020-08-19 01:16:26 +0900

    feat: MeStatusesService.kt: 実装

--- src/main/kotlin/jp/annict/lib/v1/services/me/MeStatusesService.kt
+++ src/main/kotlin/jp/annict/lib/v1/services/me/MeStatusesService.kt
@@ -1,2 +1,30 @@
 package jp.annict.lib.v1.services.me
 
+import jp.annict.lib.bases.BaseService
+import jp.annict.lib.interfaces.AnnictClient
+import jp.annict.lib.interfaces.RequestQuery
+import jp.annict.lib.v1.enums.Status
+import okhttp3.HttpUrl
+import okhttp3.Request
+
+data class MeStatuesRequestQuery(val work_id: Long?, val kind: Status?) : RequestQuery {
+
+    override fun url(builder: HttpUrl.Builder): HttpUrl {
+        return builder.apply {
+            addPathSegments("me/statues")
+
+            if(work_id != null) { addQueryParameter("work_id", work_id.toString())}
+            if(kind != null ) { addQueryParameter("kind", kind.name) }
+
+        }.build()
+    }
+}
+
+class MeStatusesService(client: AnnictClient) : BaseService(client) {
+
+    fun post(query: MeStatuesRequestQuery) : Boolean {
+        this.client.apply {
+            return (request(Request.Builder().url(query.url(getUrlBuilder())).method("post", null)).code == 204)
+        }
+    }
+}
