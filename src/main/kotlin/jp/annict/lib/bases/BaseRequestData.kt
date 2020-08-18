d52e5a0f1460e8c9adf870414100e36f66f4c106
riptakagi <smatchjp@gmail.com>
2020-08-18 11:53:11 +0900

    fix: 抽象メソッドのアルゴリズム変更

--- src/main/kotlin/jp/annict/lib/bases/BaseRequestData.kt
+++ src/main/kotlin/jp/annict/lib/bases/BaseRequestData.kt
@@ -1,18 +1,25 @@
 package jp.annict.lib.bases
 
 import jp.annict.lib.interfaces.AnnictClient
+import jp.annict.lib.utils.JsonUtil
 import okhttp3.HttpUrl
+import okhttp3.MediaType
+import okhttp3.MediaType.Companion.toMediaType
 import okhttp3.Request
 import okhttp3.RequestBody
 
 /**
  * GET以外のリクエストに利用できる
  */
-abstract class BaseRequestData(val method: String) {
+abstract class BaseRequestData(val method: String, val mediaType: MediaType, val path: String) {
 
-    abstract fun toRequestBody() : RequestBody
+    fun url(client: AnnictClient): HttpUrl {
+       return  client.getUrlBuilder().addPathSegments(path).build()
+    }
 
-    abstract fun url(client: AnnictClient): HttpUrl
+    fun toRequestBody(): RequestBody {
+        return RequestBody.create(mediaType, JsonUtil.GSON.toJson(this))
+    }
 
     fun toRequestBuilder(client: AnnictClient) : Request.Builder {
         return Request.Builder().url(url(client)).method(method, toRequestBody())
