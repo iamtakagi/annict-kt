cb9a85f830a3cf759ca02971c2d5c75017ee085c
riptakagi <smatchjp@gmail.com>
2020-08-20 04:14:52 +0900

    feat: MeFollowingActivitiesService.kt: 実装

--- src/main/kotlin/jp/annict/lib/v1/services/me/MeFollowingActivitiesService.kt
+++ src/main/kotlin/jp/annict/lib/v1/services/me/MeFollowingActivitiesService.kt
@@ -1,2 +1,69 @@
 package jp.annict.lib.v1.services.me
 
+import com.google.gson.reflect.TypeToken
+import jp.annict.lib.bases.BaseService
+import jp.annict.lib.interfaces.AnnictClient
+import jp.annict.lib.interfaces.RequestQuery
+import jp.annict.lib.interfaces.ResponseData
+import jp.annict.lib.utils.JsonUtil
+import jp.annict.lib.v1.enums.Action
+import jp.annict.lib.v1.enums.Order
+import jp.annict.lib.v1.models.Activity
+import jp.annict.lib.v1.services.ActivitiesRequestQuery
+import jp.annict.lib.v1.services.ActivitiesResponseData
+import okhttp3.HttpUrl
+import okhttp3.Request
+import okhttp3.Response
+
+data class MeFollowingActivitiesRequestQuery (
+    val fields: Array<String>?,
+    val filter_actions: Array<Action>?,
+    val filter_muted: Boolean?,
+    val page: Long?,
+    val per_page: Long?,
+    val sort_id: Order?
+): RequestQuery {
+
+    override fun url(builder: HttpUrl.Builder): HttpUrl {
+        return builder.apply {
+            addPathSegments("/me/following_activities")
+
+            if(fields != null && fields.isNotEmpty()) { addQueryParameter("fields", fields.joinToString(separator = ",")) }
+            if(filter_actions != null && filter_actions.isNotEmpty()) { addQueryParameter("filter_actions", filter_actions.joinToString(separator = ",")) }
+            if(filter_muted != null) { addQueryParameter("filter_muted", filter_muted.toString()) }
+            if(page != null) { addQueryParameter("page", page.toString()) }
+            if(per_page != null) { addQueryParameter("per_page", per_page.toString()) }
+            if(sort_id != null) { addQueryParameter("sort_id", sort_id.name) }
+
+        }.build()
+    }
+}
+
+data class MeFollowingActivitiesResponseData(
+    val activities: Array<Activity>?,
+    val total_count: Long?,
+    val next_page: Long?,
+    val prev_page: Long?
+) : ResponseData<MeFollowingActivitiesResponseData> {
+
+    constructor() : this(null, null, null, null)
+
+    override fun toDataClass(response: Response): MeFollowingActivitiesResponseData {
+        response.apply {
+            JsonUtil.JSON_PARSER.parse(body?.string()).asJsonObject.apply { return MeFollowingActivitiesResponseData (
+            JsonUtil.GSON.fromJson(getAsJsonArray("activities"), object : TypeToken<Array<Activity>>() {}.type),
+            if (get("total_count").isJsonNull) null else get("total_count").asLong,
+            if (get("next_page").isJsonNull) null else get("next_page").asLong,
+            if (get("prev_page").isJsonNull) null else get("prev_page").asLong
+        ) }
+        }
+    }
+}
+
+class MeFollowingActivitiesService(client: AnnictClient) : BaseService(client) {
+
+    fun get(query: MeFollowingActivitiesRequestQuery) : MeFollowingActivitiesResponseData {
+        this.client.apply { return MeFollowingActivitiesResponseData().toDataClass(request(Request.Builder().url(query.url(getUrlBuilder())))) }
+    }
+
+}
