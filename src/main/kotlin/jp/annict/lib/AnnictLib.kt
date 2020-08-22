684e2d3a15588acddead356b9a8a467f6127385c
notsmatch <smatchjp@gmail.com>
2020-08-22 12:32:03 +0900

    fix: OkHttpClient型の代入をしなくてもいいように修正

--- src/main/kotlin/jp/annict/lib/AnnictLib.kt
+++ src/main/kotlin/jp/annict/lib/AnnictLib.kt
@@ -6,10 +6,10 @@ import okhttp3.OkHttpClient
 
 object AnnictLib {
 
-    fun newClient(token: String, httpClient: OkHttpClient, version: AnnictVersion) : AnnictClient {
+    fun newClient(token: String, version: AnnictVersion) : AnnictClient {
         when(version) {
             AnnictVersion.V_1 -> {
-                return jp.annict.lib.v1.client.AnnictClientImpl(token, httpClient)
+                return jp.annict.lib.v1.client.AnnictClientImpl(token, OkHttpClient())
             }
         }
     }
