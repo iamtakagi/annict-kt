a120b719e21d93969306b838e3350d3593384f41
notsmatch <smatchjp@gmail.com>
2020-08-22 21:36:41 +0900

    cleanup

--- src/main/kotlin/jp/annict/lib/bases/BaseAnnictClient.kt
+++ src/main/kotlin/jp/annict/lib/bases/BaseAnnictClient.kt
@@ -1,6 +1,6 @@
 package jp.annict.lib.bases
 
-import jp.annict.lib.interfaces.AnnictClient
+import jp.annict.lib.interfaces.IAnnictClient
 import okhttp3.OkHttpClient
 
-abstract class BaseAnnictClient(val client: OkHttpClient) : AnnictClient
+abstract class BaseAnnictClient(val client: OkHttpClient) : IAnnictClient
--- src/main/kotlin/jp/annict/lib/bases/BaseService.kt
+++ src/main/kotlin/jp/annict/lib/bases/BaseService.kt
@@ -1,5 +1,5 @@
 package jp.annict.lib.bases
 
-import jp.annict.lib.interfaces.AnnictClient
+import jp.annict.lib.interfaces.IAnnictClient
 
-abstract class BaseService(val client: AnnictClient)
+abstract class BaseService(val client: IAnnictClient)
