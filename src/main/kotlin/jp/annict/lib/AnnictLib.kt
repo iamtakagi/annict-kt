ed302c13a0aecab869723b47d6109bce8fa9bbc6
notsmatch <smatchjp@gmail.com>
2020-08-22 13:35:50 +0900

    fix: メソッド変更

--- src/main/kotlin/jp/annict/lib/AnnictLib.kt
+++ src/main/kotlin/jp/annict/lib/AnnictLib.kt
@@ -1,16 +1,10 @@
 package jp.annict.lib
 
-import jp.annict.lib.enums.AnnictVersion
-import jp.annict.lib.interfaces.AnnictClient
 import okhttp3.OkHttpClient
 
 object AnnictLib {
 
-    fun newClient(token: String, version: AnnictVersion) : AnnictClient {
-        when(version) {
-            AnnictVersion.V_1 -> {
-                return jp.annict.lib.v1.client.AnnictClientImpl(token, OkHttpClient())
-            }
-        }
+    fun newClient(token: String) : jp.annict.lib.v1.client.AnnictClient {
+        return jp.annict.lib.v1.client.AnnictClient(token, OkHttpClient())
     }
 }
