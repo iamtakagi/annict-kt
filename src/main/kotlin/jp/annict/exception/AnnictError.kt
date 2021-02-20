db740043efe9a6d408365752413e2118485b6ef1
takagi <iam.takagi.dev@gmail.com>
2021-02-21 02:48:46 +0900

    cleanup files

--- src/main/kotlin/jp/annict/exception/AnnictError.kt
+++
@@ -1,9 +0,0 @@
-package jp.annict.exception
-
-/**
- * Annict error
- *
- * @property msg
- * @constructor Create empty Annict error
- */
-data class AnnictError(val msg: String? = null) : Exception(msg)
